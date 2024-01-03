a6ffa363789457139d997b6760adc7d0e42f60f6
dongyeuk <yupab3@naver.com>
2024-01-03 16:28:01 +0900

    mod ft_strdup.c (change malloc to calloc)

--- libft/ft_strdup.c
+++ libft/ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: dongyeuk <dongyeuk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 08:54:14 by dongyeuk          #+#    #+#             */
-/*   Updated: 2023/10/10 16:24:16 by dongyeuk         ###   ########.fr       */
+/*   Updated: 2024/01/03 16:26:53 by dongyeuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,7 @@ char	*ft_strdup(const char *s1)
 	size_t	ptr_size;
 
 	ptr_size = ft_strlen(s1) + 1;
-	ptr_str = (char *)malloc(ptr_size);
-	if (!ptr_str)
-		return (0);
+	ptr_str = (char *)ft_calloc(ptr_size, 1);
 	ft_strlcpy(ptr_str, s1, ptr_size);
 	return (ptr_str);
 }
