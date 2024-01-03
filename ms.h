b4739939dbb1857b1da569465f9e1aef64404348
Jaejin Lee <jaejilee@c2r3s8.42seoul.kr>
2024-01-03 14:48:42 +0900

    fix ms.h (set basic headers, set default macros)

--- ms.h
+++ ms.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ms.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dongyeuk <dongyeuk@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jaejilee <jaejilee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 14:16:32 by dongyeuk          #+#    #+#             */
-/*   Updated: 2024/01/03 14:27:33 by dongyeuk         ###   ########.fr       */
+/*   Updated: 2024/01/03 14:44:07 by jaejilee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,26 @@
 # define MS_H
 
 # include "./libft/libft.h"
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <signal.h>
+# include <unistd.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <dirent.h>
+# include <string.h>
+# include <errno.h>
+# include <sys/ioctl.h>
+# include <termios.h>
+# include <curses.h>
 
-# define FAILURE (1)
-# define SUCCESS (0)
+# define FAILURE	(1)
+# define SUCCESS	(0)
+
+# define ERR_COMMON	(1)
+# define ERR_EXCUTE	(126)
+# define ERR_CMD	(127)
 
 #endif
