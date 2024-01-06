f1c0985e03e446abcc0e44b34110776373714a5f
dongyeuk <yupab3@naver.com>
2024-01-06 15:59:38 +0900

    add ms_main.c ms_signal.c (new files)

---
+++ ms_main.c
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ms_main.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dongyeuk <dongyeuk@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/06 13:44:26 by dongyeuk          #+#    #+#             */
+/*   Updated: 2024/01/06 15:49:06 by dongyeuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ms.h"
+
+int	main(void)
+{
+	char	*str;
+
+	rl_catch_signals = 0;
+	signal(SIGINT, handle_ctrl_c);
+	signal(SIGQUIT, SIG_IGN);
+	while (1)
+	{
+		str = readline("prompt : ");
+		if (str == NULL)
+		{
+			printf("\033[1A\033[9Cexit\n");
+			exit(1);
+		}
+		if (str)
+			printf("%s\n", str);
+		else
+			break ;
+		add_history(str);
+		free(str);
+	}
+	return (0);
+}
