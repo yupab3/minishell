f2664fefeb55837ff8f57513aee34d6184ebd7fb
Jaejin Lee <jaejilee@c2r3s8.42seoul.kr>
2024-01-08 14:18:57 +0900

    mod ms.h (revise structures and enums)

--- ms.h
+++ ms.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ms.h                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dongyeuk <dongyeuk@student.42seoul.kr>     +#+  +:+       +#+        */
+/*   By: jaejilee <jaejilee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/03 14:16:32 by dongyeuk          #+#    #+#             */
-/*   Updated: 2024/01/06 15:49:00 by dongyeuk         ###   ########.fr       */
+/*   Updated: 2024/01/08 14:16:17 by jaejilee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,28 +29,6 @@
 # include <readline/readline.h>
 # include <readline/history.h>
 
-typedef enum e_symbol
-{
-	CMD,		// 0
-	LOGICAL_OP,	// 1
-	PIPE,		// 2
-	REDIRECT,	// 3
-	HERE_DOC	// 4
-}			t_symbol;
-
-typedef struct s_data
-{
-	t_symbol	*symbol;
-	char		*content;
-}			   t_data;
-
-typedef struct s_tree
-{
-	t_data			data;
-	struct s_tree	*left;
-	struct s_tree	*right;
-}			   t_tree;
-
 /* STATUS MACRO */
 # define FAILURE	(1)
 # define SUCCESS	(0)
@@ -59,6 +37,47 @@ typedef struct s_tree
 # define ERR_COMMON	(1)
 # define ERR_EXCUTE	(126)
 # define ERR_CMD	(127)
+
+/* symbol for io */
+typedef enum e_io
+{
+	INPUT,
+	OUTPUT,
+	HERE_DOC,
+	ATTACH
+}	t_io;
+
+/* symbol for data */
+typedef enum e_symbol
+{
+	CMD,
+	LOGICAL_OP,
+	PIPE
+}	t_symbol;
+
+/* list of io */
+typedef struct s_io_list
+{
+	t_io			symbol;
+	char			*content;
+	struct s_list	*next;
+}	t_io_list;
+
+/* data of tree */
+typedef struct s_data
+{
+	t_symbol	symbol;
+	char		**content;
+	t_io_list	*io;
+}	t_data;
+
+/* tree node */
+typedef struct s_tree
+{
+	t_data			data;
+	struct s_tree	*left;
+	struct s_tree	*right;
+}	t_tree;
 
 /* ms_signal.c */
 void	handle_ctrl_c(int signum);
