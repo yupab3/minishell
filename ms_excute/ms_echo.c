0bce64aa72d2b09dbf409a7c0c4dbbf66d8beec0
dongyeuk <yupab3@naver.com>
2024-01-08 16:25:21 +0900

    add ms_echo.c (built-in func echo)

--- ms_excute/ms_echo.c
+++ ms_excute/ms_echo.c
@@ -6,7 +6,30 @@
 /*   By: jaejilee <jaejilee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 12:26:10 by jaejilee          #+#    #+#             */
-/*   Updated: 2024/01/08 12:26:11 by jaejilee         ###   ########.fr       */
+/*   Updated: 2024/01/08 17:05:17 by jaejilee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../ms.h"
+
+void	ms_echo(t_data *d)
+{
+	int	i;
+	int	nl_flag;
+
+	if (d->content[1] != NULL && ft_strlen(d->content[1]) == 2
+		&& ft_strncmp(d->content[2], "-n", 2) == 0)
+	{
+		i = 1;
+		nl_flag = TRUE;
+	}
+	else
+	{
+		i = 0;
+		nl_flag = FALSE;
+	}
+	while (d->content[++i] != 0)
+		printf("%s ", d->content[i]);
+	if (nl_flag == FALSE)
+		printf("\n");
+}
