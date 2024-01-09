5909d3398946e002f76655ecbb32ec426a200521
dongyeuk <yupab3@naver.com>
2024-01-09 14:59:58 +0900

    mod ms_init_tree.c (remove duplicate init code)

--- ms_parse/ms_init_tree.c
+++ ms_parse/ms_init_tree.c
@@ -6,8 +6,16 @@
 /*   By: dongyeuk <dongyeuk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/08 12:34:45 by dongyeuk          #+#    #+#             */
-/*   Updated: 2024/01/08 12:34:49 by dongyeuk         ###   ########.fr       */
+/*   Updated: 2024/01/09 13:48:56 by dongyeuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ms.h"
+#include "../ms.h"
+
+t_tree	*create_tree(void)
+{
+	t_tree	*b_tree;
+
+	b_tree = (t_tree *)ft_calloc(1, sizeof(t_tree));
+	return (b_tree);
+}
